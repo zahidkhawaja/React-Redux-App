8fadf8757624a9f6e6148101fbe819a5abd1a636
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-03-18 13:59:15 -0400

    Add activity type to render

--- app/src/components/Activity.js
+++ app/src/components/Activity.js
@@ -4,8 +4,9 @@ import { connect } from "react-redux";
 const Activity = props => {
     return (
         <div className = "activity">
-            <h3>Activity:</h3>
+            <h3>Random Activity:</h3>
             <p>{props.activity.activity}</p>
+            <p>Type: {props.activity.type}</p>
         </div>
     );
 };
